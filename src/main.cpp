39a393218d7bd2d9484eb382b5665708922ccfa3
Haoling <maya@fei-yen.jp>
2021-05-22 20:58:08 +0900

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include <VarSpeedServo.h>
+VarSpeedServo myservo;
+
+#define PIN_SERVO 9
+#define SPEED 30
 
 void setup() {
+    myservo.attach(PIN_SERVO);
 }
 
 void loop (){
+    myservo.write(180, SPEED, true);
+    myservo.wait();
+    delay(200);
+    myservo.write(0, SPEED, true);
+    myservo.wait();
+    delay(1000);
 }
